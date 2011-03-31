4dd76aada82fb163d9a37fbc197a08dc5971c149
DJ Brown <dj@hcv.cc>
2011-03-31 11:49:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zorillanet::Application.config.secret_token = 'fdb226858fbb8ec1d8479538f16c064191dd31a9847cb89f4dcc474c6ad251be07b376309483213e675c6bc510286c9cf0f2c1bac7a251a394230d4e4f91dc44'
